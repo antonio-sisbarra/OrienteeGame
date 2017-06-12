a8bc5bf762d978f14ace8ed2c220480fb73f8ba5
Antonio Sisbarra <sisbarra@gmail.com>
2017-06-12 19:00:18 +0200

    Aggiunta activity con le tre sezioni (gioco, percorsi, storia)

--- app/src/main/java/com/sisbarra/orienteegame/MainActivity.java
+++ app/src/main/java/com/sisbarra/orienteegame/MainActivity.java
@@ -1,13 +1,200 @@
 package com.sisbarra.orienteegame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+//Codice dell'activity principale che contiene le tre sezioni della app
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Il {@link android.support.v4.view.PagerAdapter} che fornirà
+     * i fragments per ognuna delle sezioni. Uso una sottoclasse di
+     * {@link FragmentPagerAdapter}, che mantiene ogni fragment
+     * caricato in memoria.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * Il {@link ViewPager} che contiene i contenuti.
+     */
+    private ViewPager mViewPager;
+
+    private Toolbar mToolbar;
+    private TabLayout mTabLayout;
+
+    private static final int mNTabs = 3;
+
+    /* Icone per modalità GAME, PERCORSI DA FARE, STORIA*/
+    private int[] mTabIcons = {
+            android.R.drawable.ic_menu_compass,
+            android.R.drawable.ic_menu_mapmode,
+            android.R.drawable.ic_menu_myplaces
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        // Crea l'adapter che restituirà un fragment per ognuno delle tre
+        // sezioni primarie dell'activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Setta il viewpager con l'adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mTabLayout = (TabLayout) findViewById(R.id.tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        //Setta le icone per le tab
+        setupTabIcons();
+
+        //TODO:DA COMPLETARE
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    //Setta le icone e il testo per le tab
+    private void setupTabIcons() {
+
+        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOne.setText(R.string.tab_one);
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, mTabIcons[0], 0, 0);
+        mTabLayout.getTabAt(0).setCustomView(tabOne);
+
+        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabTwo.setText(R.string.tab_two);
+        tabTwo.setCompoundDrawablesWithIntrinsicBounds(0, mTabIcons[1], 0, 0);
+        mTabLayout.getTabAt(1).setCustomView(tabTwo);
+
+        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabThree.setText(R.string.tab_three);
+        tabThree.setCompoundDrawablesWithIntrinsicBounds(0, mTabIcons[2], 0, 0);
+        mTabLayout.getTabAt(2).setCustomView(tabThree);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * Un {@link FragmentPagerAdapter} che restituisce il fragment corrispondente a
+     * una sezione.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        //Restituisce il fragment per una data posizione
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        //Restituisce il numero di pagine
+        public int getCount() {
+            // Show 3 total pages.
+            return mNTabs;
+        }
+
+        @Override
+        //Restituisce il titolo di una pagina data una posizione
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getResources().getString(R.string.tab_one);
+                case 1:
+                    return getResources().getString(R.string.tab_two);
+                case 2:
+                    return getResources().getString(R.string.tab_three);
+            }
+            return null;
+        }
     }
 }
